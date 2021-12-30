c77d934bbd0decd95b4da4c78d883250e9564f5a
Manolis Ntelopoulos <mdelopo@gmail.com>
2021-12-30 22:56:31 +0200

    removed examples, added basic features: windows, map, relaod

--- main.c
+++ main.c
@@ -1,64 +1,62 @@
-#include "maputlis.h"
+#include "maputils.h"
+#include "terminalutils.h"
 #include <curses.h>
 
-void draw_borders(WINDOW *screen) {
-    int x, y, i;
-    getmaxyx(screen, y, x);
-    mvwprintw(screen, 0, 0, "+");
-    mvwprintw(screen, y - 1, 0, "+");
-    mvwprintw(screen, 0, x - 1, "+");
-    mvwprintw(screen, y - 1, x - 1, "+");
-    for (i = 1; i < (y - 1); i++) {
-        mvwprintw(screen, i, 0, "|");
-        mvwprintw(screen, i, x - 1, "|");
-    }
-    for (i = 1; i < (x - 1); i++) {
-        mvwprintw(screen, 0, i, "-");
-        mvwprintw(screen, y - 1, i, "-");
-    }
-}
-
+int file_rows = 0, file_columns = 0;
 
 int main() {
-    location loc;
-    read_location_from_file("island_default.csv", 5, 4, &loc);
-    print_location(&loc);
+    //location loc;
+    //read_location_from_file("island_default.csv", 9, 4, &loc);
+    //print_location(&loc);
+    location** map = load_map_from_file("island_default.csv");
+    //printf("Rows: %d Columns: %d", file_rows, file_columns);
 
     initscr();
+    start_color();
+    curs_set(0);
+    cbreak();
+    noecho();
 
-    addstr("-----------------\n| codedrome.com |\n| ncurses Demo  |\n-----------------\n\n");
+    WINDOW *console_box = newwin(10, 10, 0, 0);
+    WINDOW *console_window = newwin(10, 10, 0, 0);
+    WINDOW *map_box = newwin(10, 10, 0,0);
+    WINDOW *map_window = newwin(10, 10, 0, 0);
+
+    scrollok(console_window, TRUE);
     refresh();
+    resize_window(console_box, console_window, map_box, map_window);
+    wprintw(console_window, "Calling newwin creates and returns a pointer to a new window with the given number of lines and columns. The upper left-hand corner of the window is at line begin_y, column begin_x. If either nlines or ncols is zero, they default to LINES - begin_y and COLS - begin_x. A new full-screen window is created by calling newwin(0,0,0,0).");
+    wrefresh(console_window);
+    //wprintw(map_window, "Screen window is created by calling newwin(0,0,0,0).");
+    wrefresh(map_window);
 
-    //printing();
+    init_pair(1,COLOR_WHITE, COLOR_CYAN);
+    init_pair(2,COLOR_WHITE, COLOR_GREEN);
+    init_pair(3,COLOR_BLACK, COLOR_WHITE);
+    init_pair(4,COLOR_BLACK, COLOR_YELLOW);
 
-    // moving_and_sleeping();
+    wbkgd(map_window, COLOR_PAIR(1));
+    wrefresh(map_window);
 
-    //colouring();
-    WINDOW *score = newwin(20, 20, 0, 0);
-    WINDOW *field = newwin(20, 20, 0, 0);
+    //print_map(map_window, map);
 
-    int parent_x, parent_y = 0, new_x, new_y;
-    int score_size = 3; // ...
-    draw_borders(field);
-    draw_borders(score);
     while(1){
-        getmaxyx(stdscr, new_y, new_x);
-        if (new_y != parent_y || new_x != parent_x) {
-            parent_x = new_x; parent_y = new_y;
-            wresize(field, new_y - score_size, new_x);
-            wresize(score, score_size, new_x);
-            mvwin(score, new_y - score_size, 0);
-            wclear(stdscr);
-            wclear(field);
-            wclear(score);
-            draw_borders(field);
-            draw_borders(score);
-        } // draw to our windows
-        mvwprintw(field, 1, 1, "Field");
-        mvwprintw(score, 1, 1, "Score"); // refresh each window
-        wrefresh(field);
-        wrefresh(score);
-    } // ...
+    char input = getch();
+        switch (input){
+            case 'r':
+                resize_window(console_box, console_window, map_box, map_window);
+                break;
+            case 'c':
+                werase(map_window);
+                wrefresh(map_window);
+                break;
+            case 'm':
+                print_map(map_window, map);
+                break;
+            default:
+                break;
+        }
+    }
 
     endwin();
 
