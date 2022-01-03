0a515755bbe3e858ccae9646ae548e87d132140b
Manolis Ntelopoulos <mdelopo@gmail.com>
2022-01-03 22:48:54 +0200

    added basic elementsutils

---
+++ elementsutils.c
@@ -0,0 +1,57 @@
+#include "elementsutils.h"
+#include "maputils.h"
+
+#include <curses.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_element(element* element)
+{
+    printf("\"element\" struct:\n");
+    printf("Alias: %c\n", element->alias);
+    printf("Function: %c\n", element->function);
+    printf("Function Amount: %f\n", element->function_amount);
+    printf("Text: %s\n", element->text);
+}
+
+void read_elements_from_file(const char* filename, element* elements)
+{
+    FILE* stream = fopen(filename, "r");
+    char line[1024];
+    int i = 0;
+    while (fgets(line, 1024, stream))
+    {
+        sscanf(line, "%c;%c;%f;%[^\n]s", &elements[i].alias, &elements[i].function, &elements[i].function_amount, elements[i].text);
+        i++;
+    }
+    fclose(stream);
+}
+
+element* allocate_memory_for_elements(int y)
+{
+    element *elements = malloc(y * sizeof(element));
+    return elements;
+}
+
+void free_memory_for_elements(element* elements, int y)
+{
+    free(elements);
+}
+
+element* load_elements_from_file(const char* filename){
+    FILE* stream = fopen(filename, "r");
+    char line[1024];
+    int y =0;
+    while (fgets(line, 1024, stream)){
+        y++;
+    }
+    fclose(stream);
+
+    element *elements = allocate_memory_for_elements(y);
+
+    read_elements_from_file(filename, elements);
+
+    return elements;
+}
---
+++ elementsutils.h
@@ -0,0 +1,21 @@
+#ifndef TREASURE_ISLAND_ELEMENTSUTILS_H
+#define TREASURE_ISLAND_ELEMENTSUTILS_H
+
+#include <curses.h>
+
+typedef struct {
+    char alias;
+    char function;
+    float function_amount;
+    char text[500];
+} element;
+
+void print_element(element* e);
+
+void read_elements_from_file(const char* filename, element* elements);
+element* allocate_memory_for_elements(int y);
+void free_memory_for_elements(element* elements, int y);
+element* load_elements_from_file(const char* filename);
+
+
+#endif
